e4aeef85d9bcf78782e9915e3d473f3bbe1cc501
Santiago Villarreal Arley <santivillarley1010@gmail.com>
2023-08-29 16:18:55 -0600

    Adding notifications model

---
+++ models/reminder.ts
@@ -0,0 +1,46 @@
+// models/reminder.ts
+
+import { DataTypes, Model, Sequelize } from 'sequelize';
+
+class Reminder extends Model {
+  public id!: number;
+  public Date!: Date;
+  public description!: string;
+  public priority!: string;
+  public PersonEmail!: string;
+
+  static initModel(sequelize: Sequelize) {
+    return Reminder.init({
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      Date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      priority: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      PersonEmail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: 'person', // 'persons' refers to table name
+          key: 'email',
+        },
+      },
+    }, {
+      tableName: 'reminders',
+      sequelize: sequelize,
+    });
+  }
+}
+
+export default Reminder;
