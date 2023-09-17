4fe4ac550ef4c0d3e4ad89e5e4753bc2d376735b
Santiago Villarreal Arley <santivillarley1010@gmail.com>
2023-09-17 07:40:34 -0600

    Adding scheduled-notifications
    Aded:
    -NodeCron for managing the time
    -momentTimezone for managing the local time
    -the function to schedule a notfication

--- services/notification.service.ts
+++ services/notification.service.ts
@@ -1,17 +1,36 @@
 import { Notification } from '../models/notification';
 import fcmService from './fcmService';
+import cron from "node-cron";
+import moment from "moment-timezone"
 
 class NotificationService {
     // Create a new notification and send a push notification.
     async createNotification(data: any): Promise<Notification> {
         const notification = await Notification.create(data);
+        const { title, description, date, tokenDevice } = data;
 
-        // Send a push notification if the user has an FCM token.
-        // if (notification.tokenDevice) {
-        //     const title = 'New Notification';
-        //     const body = 'You have a new notification in the app.';
-        //     await fcmService.sendNotification(notification.tokenDevice, title, body);
-        // }
+        const timezone = 'America/Costa_Rica';
+
+        const currentDate = moment.tz(timezone);
+        const scheduledDate = moment.tz(date, timezone);
+
+        // Check if the scheduled date is in the future
+        console.log(scheduledDate,currentDate);
+        if (scheduledDate.isAfter(currentDate)) {
+            // Convert the scheduled date to a cron expression
+            const cronExpression = `${scheduledDate.minute()} ${scheduledDate.hour()} ${scheduledDate.date()} ${scheduledDate.month() + 1} *`;
+    
+            cron.schedule(cronExpression, async () => {
+                if (tokenDevice) {
+                    await fcmService.sendNotification(tokenDevice, title, description);
+                }
+            });
+        } else {
+            // Send the notification immediately if the date is not in the future
+            if (tokenDevice) {
+                await fcmService.sendNotification(tokenDevice, title, description);
+            }
+        }
 
         return notification;
     }
